0aefcf820c74a45add72d20f9ce6c97d2a6da632
Ricardo Estrada Villavicencio <rickyv815@gmail.com>
2023-03-28 14:03:26 -0700

    Completed style and code for Home Page SideNav

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 import CoverPic from "../resources/newPortfolioCover.jpg"
+import ThemeImage from "../resources/cover.jpeg"
 // 
 const SideNav = () => {
     return (
         <>
         <div className="header">
-        <img id="headCover" src={CoverPic}/>
+        <img id="headShot" src={CoverPic}/>
             <h3 className="intro">Ricardo Estrada Villavicencio | Software Engineer</h3>
         </div>
         <div className="sideNav">
-            <Link to="/">Home</Link>
-            <Link to="/AboutMe">About Me</Link>
-            <Link to="/Background">Background</Link>
-            <Link to="/Skills&Experience">Skills & Experience</Link>
-            <Link to="/ContactMe">Contact Me</Link>
+            <img id="theme" src={ThemeImage} />
+            <Link className="navButton" to="/">Home</Link>
+            <Link className="navButton" to="/AboutMe">About Me</Link>
+            <Link className="navButton" to="/Background">Background</Link>
+            <Link className="navButton" to="/Skills&Experience">Skills & Experience</Link>
+            <Link className="navButton contactButton" to="/ContactMe">Contact Me</Link>
         </div>
         </>
     )
