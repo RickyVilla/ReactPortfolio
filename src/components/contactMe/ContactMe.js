277429231077e3d1edcbff71188259765204b06a
Ricardo Estrada Villavicencio <rickyv815@gmail.com>
2023-03-30 14:00:29 -0700

    Completed Contact Me Side Nav.

--- src/components/contactMe/ContactMe.js
+++ src/components/contactMe/ContactMe.js
@@ -6,31 +6,26 @@ import CallMe from "../../resources/callMe.webp"
 function ContactMe () {
     return (
         <div className="components">
- 
             <div className="content" id="linksPage">
                 <h3 id="ProjectsTitle">Review my Projects and Connect with me Here:</h3>
                 <div>
                     <a target="_new" href="https://github.com/RickyVilla">
-                        GitHub
-                        <img className="icons" src={GitHub} />
+                        GitHub<img className="icons" src={GitHub} />
                     </a>
                 </div>
                 <div>
                     <a target="_new" href="https://www.linkedin.com/in/r-villavicencio">
-                        LinkedIn
-                        <img className="icons" src={LinkedIn} />
+                        LinkedIn<img className="icons" src={LinkedIn} />
                     </a>    
                 </div>
                 <div>                    
-                    <a target="_new">
-                        GMail
-                        <img className="icons" src={Gmail} />
+                    <a target="_new" href="">
+                        GMail<img className="icons" src={Gmail} />
                     </a>
                 </div>
                 <div>                   
                     <a target="_new" href="">
-                    Request-Call 
-                        <img className="icons" src={CallMe} />
+                    Request-Call<img className="icons" src={CallMe} />
                     </a></div>
             </div>
             {/* <div className="footer">
